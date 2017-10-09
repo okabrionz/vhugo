3e483d093c87256e434b51ce655f7209d42e0106
Bo Wang <bo@yieldmo.com>
2017-10-09 16:32:42 -0400

    new yieldmo

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 // JS Goes here - ES6 supported
 $(function() {
-  var mrbo = {
+  var yieldmo = {
     modules: {
       about: require('./modules/_about.js')
     },
 
     init: function() {
-      var greetings = this.modules.about('mr.bo & co');
+      var greetings = this.modules.about('new yieldmo');
 
       // Say hello
       console.log(greetings);
     }
   };
 
-  mrbo.init();
+  yieldmo.init();
 });
